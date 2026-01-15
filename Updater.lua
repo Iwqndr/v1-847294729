ff4c8f5548ee1be3a68b3730a8818765f8b19624
Iwqndr <133339397+Iwqndr@users.noreply.github.com>
2026-01-14 23:24:03 -0800

    Adjust wait times for update checks and loading

--- Updater.lua
+++ Updater.lua
@@ -1,9 +1,9 @@
 local mainUrl = "https://raw.githubusercontent.com/Iwqndr/v1-847294729/main/PLU.Lua?t=" .. tick()
 
 print("Connecting to server...")
-task.wait(0.5)
+task.wait(1.4)
 print("Checking for updates.")
-task.wait(0.5)
+task.wait(1.6)
 
 local success, code = pcall(function()
     return game:HttpGet(mainUrl)
@@ -11,7 +11,7 @@ end)
 
 if success then
     print("No updates found, Your all good!")
-    task.wait(0.5)
+    task.wait(2)
     print("Loading PLU.Lua...")
     loadstring(code)()
 else
